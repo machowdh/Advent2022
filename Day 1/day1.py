fae6b8b586acce53dfcc343a8f8f2b5d603508c4
Mutasim Chowdhury <35827063+machowdh@users.noreply.github.com>
2022-12-01 20:36:40 -0600

    Removed reverse logic

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -3,7 +3,7 @@ from aocd import get_data, transforms
 data = get_data(day=1, year=2022)
 lines = [l.split('\n') for l in data.split('\n\n')]
 
-elves = sorted([sum(list(map(int, e))) for e in lines], reverse=True)
-print(elves[0])
-print(sum(elves[:3]))
+elves = sorted([sum(list(map(int, e))) for e in lines])
+print(elves[-1])
+print(sum(elves[-3:]))
 
