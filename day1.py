0c020e8bc31a079384be56fbd22b6616cad0bc51
Mutasim Chowdhury <35827063+machowdh@users.noreply.github.com>
2022-12-01 21:46:48 -0600

    Cleaning up filestructure

--- day1.py
+++ day1.py
@@ -4,6 +4,7 @@ data = get_data(day=1, year=2022)
 lines = [l.split('\n') for l in data.split('\n\n')]
 
 elves = sorted([sum(list(map(int, e))) for e in lines])
-print(elves[-1])
-print(sum(elves[-3:]))
+
+print(f'Part One: {elves[-1]}')
+print(f'Part Two: {sum(elves[-3:])}')
 
---
+++ day2.py
@@ -0,0 +1,3 @@
+from aocd import get_data, transforms
+
+data = get_data(day=1, year=2022)
