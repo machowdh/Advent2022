33295d28e44df8908bec95f5db8386fde9e45c5e
Mutasim Chowdhury <35827063+machowdh@users.noreply.github.com>
2022-12-02 23:34:39 -0600

    slight cleanup on day 3

--- day3.py
+++ day3.py
@@ -1,6 +1,6 @@
 from aocd import get_data, transforms
-import string
 from functools import reduce
+import string
 
 data = get_data(day=3, year=2022)
 
@@ -10,4 +10,4 @@ groups = [lines[i:i+3] for i in range(0, len(lines), 3)]
 char_value = {c: i for i, c in enumerate(string.ascii_letters, start=1)}
 
 print(f'Part One: {sum(char_value[(set(l[:len(l)//2]) & set(l[len(l)//2:])).pop()] for l in lines)}')
-print(f'Part One: {sum(char_value[reduce(lambda x, y : set(x) & set(y), g).pop()] for g in groups)}')
+print(f'Part Two: {sum(char_value[reduce(lambda x, y : set(x) & set(y), g).pop()] for g in groups)}')
