7325354ec62222920a8c667455f38744cef117f1
Mutasim Chowdhury <35827063+machowdh@users.noreply.github.com>
2022-12-02 19:00:19 -0600

    Day 2 cleaned

--- day2.py
+++ day2.py
@@ -1,60 +1,46 @@
 from aocd import get_data, transforms
-from collections import defaultdict
+from collections import defaultdict, namedtuple
+
 data = get_data(day=2, year=2022)
 lines = transforms.lines(data)
 
-lines = [(p[0], p[2]) for p in lines]
-# A, X : Rock
-# B, Y: Paper
-# C, Z: Scissors
+state = ['A', 'B', 'C']
 
-shape = {'A': 1, 'B': 2, 'C': 3}
-h = {'X': 'A', 'Y': 'B', 'Z': 'C'}
-win_con = {('C', 'A'), ('B', 'C'), ('A', 'B')}
-p2_con = {
-    ('A', 'X') : ('A', 'C'),
-    ('A', 'Y') : ('A', 'A'),
-    ('A', 'Z') : ('A', 'B'),
-    ('B', 'X') : ('B', 'A'),
-    ('B', 'Y') : ('B', 'B'),
-    ('B', 'Z') : ('B', 'C'),
-    ('C', 'X') : ('C', 'B'),
-    ('C', 'Y') : ('C', 'C'),
-    ('C', 'Z') : ('C', 'A'),
-}
+class Game:
+    
+    def __init__(self, char):
+        self.char = char
+        state_index = state.index(char)
 
-# Rock, Paper, Scissors
-# A, B, C
+        self.wins = state[state_index - 1]
+        self.loses = state[(state_index + 1) % 3]
+        self.value = state_index + 1
 
+    def __eq__(self, other):
+        return self.char == other
 
-# Win Conditions : Rock < Paper, Paper < Scissors, Scissors < Rock
-#                  A < B, B < C, C < A
-#
-
-
-# Win Condition: Player 2 is one index to the right
-# Lose Condition: Player 1 is one index to the left
-# Draw Condition: Same Index
-
-# 3 + 1 is 4
-# 4 % 3 is 1
+    def __repr__(self):
+        return f'{self.char}'
 
 def score_game(lines, part_2=False):
     score = 0
-    for p1, p2 in lines:
-        if not part_2:
-            p2 = h[p2]
+    for p1, _, p2 in lines:
+        player_one = Game(p1)
+        if part_2:
+            if p2 == 'X':
+                player_two = Game(player_one.loses)
+            elif p2 == 'Y':
+                player_two = Game(player_one.wins)
+            else:
+                player_two = Game(player_one)
         else:
-            (p1, p2) = p2_con[(p1, p2)]
-
-        if p1 == p2:
-            score += shape[p2] + 3
-        elif (p1, p2) in win_con:
-            score += 6 + shape[p2]
-        else:
-            score += shape[p2]
+            player_two = Game(chr(ord(p2) - 23))
+        score += player_two.value
+        if player_one == player_two:
+            score += 3
+        elif player_two.wins == player_one.char:
+            score += 6
     return score
-
 
 p1 = score_game(lines)
 p2 = score_game(lines, True)
